b91bc1afc16bad2977630054f291c38534cf78dd
zoeypark <mary990t@gmail.com>
2023-08-03 22:32:15 +0900

    create

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -6,7 +6,6 @@ import com.in28minutes.learnspringframework.game.MarioGame;
 public class AppGamingBasicJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		var marioGame = new MarioGame();
 		var gameRunner = new GameRunner(marioGame);
 		gameRunner.run();
